29e1775052f00e930fd1fe554cbe05d7d2e29ea5
KUMAR Vishal <vishal.kumar2@soprasteria.com>
2024-01-21 18:30:03 +0530

    test message from vishal

--- team-services-ui/src/app/core/config/app-constants.ts
+++ team-services-ui/src/app/core/config/app-constants.ts
@@ -2,4 +2,6 @@ export class AppConstants {
     public static readonly USER_INFO = "userInfo";
     public static readonly DATA_SAVED = "dataSaved";
     public static readonly TrainingData_Saved="trainingDataSaved"
+
+    // added new constants here
 }
