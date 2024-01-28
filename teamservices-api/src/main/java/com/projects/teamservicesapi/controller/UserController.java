68d38e531ec31596a7a4a241c83636b9b14e99f4
Paridhi <paridhigoyal23@gmail.com>
2024-01-28 20:41:43 +0530

    user api final

--- teamservices-api/src/main/java/com/projects/teamservicesapi/controller/UserController.java
+++ teamservices-api/src/main/java/com/projects/teamservicesapi/controller/UserController.java
@@ -3,6 +3,7 @@ package com.projects.teamservicesapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ public class UserController {
 	public User addUser(@RequestBody User user) {
 		return userService.saveUser(user);
 	}
-	
+	@CrossOrigin(origins = "http://localhost:4200/")
 	@GetMapping
 	public List<User> getAllUsers() {
 		return userService.getUsers();
